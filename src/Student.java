7536315ebd47619a213b3835212e2739e5bed9b5
TodorYanakiev <todoryanakiev395@gmail.com>
2023-11-04 19:57:25 +0200

    Student modified

--- src/Student.java
+++ src/Student.java
@@ -14,7 +14,7 @@ public class Student extends Human {
     }
 
     public void doNoHomeworkPunishment(){
-        for (int i = 0; i < 40; i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.println("I will not miss homework again!");
         }
     }
